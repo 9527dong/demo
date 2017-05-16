512f37d30375ff4b68ce601a6ec3d2ba47e678ab
Wang Dong <952764934@qq.com>
2017-05-16 22:13:37 +0800

    更新提交

--- mybatis-curd/target/classes/sql/init.sql
+++
@@ -1,9 +0,0 @@
-Create TABLE `user` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `userName` varchar(50) DEFAULT NULL,
-  `userAge` int(11) DEFAULT NULL,
-  `userAddress` varchar(200) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ;
-
-Insert INTO `user` VALUES ('1', 'summer', '100', 'shanghai,pudong');
