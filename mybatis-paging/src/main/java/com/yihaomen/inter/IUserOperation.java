b8a86c1bf80a7c8b0bf8c29b81f6c7daac12c384
Dong <952764934@qq.com>
2017-05-22 18:10:28 +0800

    新建分页项目

---
+++ mybatis-paging/src/main/java/com/yihaomen/inter/IUserOperation.java
@@ -0,0 +1,25 @@
+package com.yihaomen.inter;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Param;
+
+import com.yihaomen.model.Article;
+import com.yihaomen.model.User;
+import com.yihaomen.util.PageInfo;
+
+
+public interface IUserOperation {
+    
+	public User selectUserByID(int id);
+	public List<User> selectUsers(String userName);	
+	public void addUser(User user);
+	public void updateUser(User user);
+	public void deleteUser(int id);
+	
+	public List<Article> getUserArticles(int id);
+	
+	public List<Article> selectArticleListPage(@Param("page") PageInfo page, @Param("userid") int userid);
+	
+	
+}
---
+++ mybatis-paging/src/main/webapp/index.jsp
@@ -0,0 +1,16 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+         pageEncoding="UTF-8"%>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<%
+    String basePath = request.getScheme()+"://"+request.getServerName()+":"+request.getServerPort()+request.getContextPath()+"/";
+%>
+<html>
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <title>Insert title here</title>
+</head>
+<body>
+<a href="<%= basePath%>article/list">不分页的list</a><br />
+<a href="<%= basePath%>article/pagelist">分页list测试</a>
+</body>
+</html>
